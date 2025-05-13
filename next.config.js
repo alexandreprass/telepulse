9b9c16331d69114a1ccf531777969c0aaf1a5d62
alexandreprass <162365360+alexandreprass@users.noreply.github.com>
2025-05-13 09:38:46 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.alias['@/lib'] = './lib';
+    return config;
+  },
 };
+
+module.exports = nextConfig;
