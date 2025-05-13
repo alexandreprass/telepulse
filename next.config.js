235e772d89b0d5de3578b1d5d209f927abdccf86
alexandreprass <162365360+alexandreprass@users.noreply.github.com>
2025-05-13 11:07:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
   webpack: (config) => {
     config.resolve.alias['@/lib'] = './lib';
-    // Mockar módulos Node.js no cliente
     config.resolve.fallback = {
       fs: false,
       net: false,
